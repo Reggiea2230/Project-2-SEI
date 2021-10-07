2b3ebcc6f353768b39b5f9ba1c2d2c359a1f43f0
Reggie <reginaldallonce@gmail.com>
2021-10-07 13:18:53 -0400

    updating

--- controllers/review.js
+++ controllers/review.js
@@ -36,12 +36,20 @@ function deleteReviews(req, res) {
   }
 
   function updateReviews(req, res){
+      // Note the cool "dot" syntax to query on the property of a subdoc
     Sneaker.findOne({'review._id': req.params.id}, function(err, sneaker){
+        // Find the comment subdoc using the id method on Mongoose arrays
+        // https://mongoosejs.com/docs/subdocs.html
         const reviewSubdoc = sneaker.review.id(req.params.id);
+         // Ensure that the comment was created by the logged in user
         if(!reviewSubdoc.userId.equals(req.user._id)) return res.redirect(`/sneakers/${sneaker._id}`);
+        // Update the text of the comment
         reviewSubdoc.descrip = req.body.descrip;
+        // Save the updated book
         reviewSubdoc.rating = parseInt(req.body.rating);
+        console.log("review subdoc", reviewSubdoc)
         sneaker.save(function(err){
+            // Redirect back to the book's show view
             res.redirect(`/sneakers/${sneaker._id}`);
         });
     });
--- routes/sneaker.js
+++ routes/sneaker.js
@@ -4,10 +4,14 @@ var router = require('express').Router();
 var shoeCtrl = require('../controllers/sneaker');
 
 
-router.get('/', shoeCtrl.index);
-router.get('/new', shoeCtrl.new);
-router.get('/:id', shoeCtrl.show);
-router.post('/', shoeCtrl.create);
+router.get('/', isLoggedIn, shoeCtrl.index);
+router.get('/new', isLoggedIn, shoeCtrl.new);
+router.get('/:id', isLoggedIn, shoeCtrl.show);
+router.post('/', isLoggedIn, shoeCtrl.create);
 
+function isLoggedIn(req, res, next){
+    if (req.isAuthenticated()) return next();
+    res.redirect('/auth/google');
+}
 
 module.exports = router;
